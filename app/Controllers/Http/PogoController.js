9b2a3e39a79f82d05199022e9fdb2b4e1c2f07f6
angelfabrico09 <163194@ids.upchiapas.edu.mx>
2019-07-02 19:54:03 -0500

    add status 8

--- app/Controllers/Http/PogoController.js
+++ app/Controllers/Http/PogoController.js
@@ -48,7 +48,7 @@ class PogoController {
     const namepizza = request.input('namepizza')
     const cantidad = request.input('cantidad')
     const tamanio =request.input('tamanio')
-    //const status =request.input('status')
+    const status =request.input('status')
    
     const pago = new Pago()
     pago.nameclient = nameclient
@@ -58,7 +58,7 @@ class PogoController {
     pago.namepizza=namepizza
     pago.cantidad=cantidad
     pago.tamanio=tamanio
-   // pago.status=status
+    pago.status=status
     await pago.save()
     return response.json(pago)
 
@@ -126,7 +126,7 @@ class PogoController {
     const namepizza = request.input('namepizza')
     const cantidad = request.input('cantidad')
     const tamanio =request.input('tamanio')
-   // const status =request.input('status')
+    const status =request.input('status')
 
     let pago = await Pago.find(params.id)
 
@@ -137,7 +137,7 @@ class PogoController {
     pago.namepizza=namepizza
     pago.cantidad=cantidad
     pago.tamanio=tamanio
-   // pago.status=status
+    pago.status=status
     await pago.save()
     return response.json(pago)
   }
