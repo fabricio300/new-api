06fd83aa44441f2adc68101d5314d717990edc75
angelfabrico09 <163194@ids.upchiapas.edu.mx>
2019-07-02 19:50:30 -0500

    add status 7

---
+++ database/migrations/1562114995821_pagos_schema.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class PagosSchema extends Schema {
+  up () {
+    this.table('pagos', (table) => {
+      // alter table
+      table.string('status')
+    })
+  }
+
+  down () {
+    this.table('pagos', (table) => {
+      // reverse alternations
+    })
+  }
+}
+
+module.exports = PagosSchema
