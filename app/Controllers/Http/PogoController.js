b270cbb4b953c293f3aca3b547d8a216b63dd722
angelfabrico09 <163194@ids.upchiapas.edu.mx>
2019-07-02 20:50:03 -0500

    first commit

--- app/Controllers/Http/PogoController.js
+++ app/Controllers/Http/PogoController.js
@@ -48,7 +48,7 @@ class PogoController {
     const namepizza = request.input('namepizza')
     const cantidad = request.input('cantidad')
     const tamanio =request.input('tamanio')
-  //  const statuspizza =request.input('statuspizza')
+    const statuspizza =request.input('statuspizza')
    
     const pago = new Pago()
     pago.nameclient = nameclient
@@ -58,7 +58,7 @@ class PogoController {
     pago.namepizza=namepizza
     pago.cantidad=cantidad
     pago.tamanio=tamanio
-   // pago.statuspizza=statuspizza
+   pago.statuspizza=statuspizza
 
 
     await pago.save()
@@ -128,7 +128,7 @@ class PogoController {
     const namepizza = request.input('namepizza')
     const cantidad = request.input('cantidad')
     const tamanio =request.input('tamanio')
-    //const statuspizza =request.input('statuspizza')
+    const statuspizza =request.input('statuspizza')
 
     let pago = await Pago.find(params.id)
 
@@ -139,7 +139,7 @@ class PogoController {
     pago.namepizza=namepizza
     pago.cantidad=cantidad
     pago.tamanio=tamanio
-    //pago.statuspizza=statuspizza
+    pago.statuspizza=statuspizza
     await pago.save()
     return response.json(pago)
   }
--- app/Models/Pagos2.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
-
-class Pagos2 extends Model {
-}
-
-module.exports = Pagos2
